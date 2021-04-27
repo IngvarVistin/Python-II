7d395b1c9e4d88116702606b3390c1a86a1d9fef
Ingvar <Dimon_chick@mail.ru>
2021-04-27 17:41:18 +0900

    Lesson_1_1 finish

--- Lesson_1/Lesson_1_1.py
+++ Lesson_1/Lesson_1_1.py
@@ -1,8 +1,5 @@
 ''' 1. Найти сумму и произведение цифр трехзначного числа, которое вводит пользователь. '''
 
-number = str(input("Введите трёхзначное число: "))
-summ = int(number[0]) + int(number[1]) + int(number[2])
-composition = int(number[0]) * int(number[1]) * int(number[2])
 number = int(input("Введите трёхзначное число: "))
 x = number//100
 y = number % 100 // 10
