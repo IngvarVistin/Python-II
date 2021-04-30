a34acad964c322604a00a228cd79966cd891cb13
Ingvar <Dimon_chick@mail.ru>
2021-04-30 21:42:56 +0900

    II Lesson_2_2 recursion

--- Lesson__2/II Lesson_2_2.py
+++ Lesson__2/II Lesson_2_2.py
@@ -3,18 +3,17 @@
 Например, если введено число 34560, то у него 3 четные цифры (4, 6 и 0) и 2 нечетные (3 и 5).
 '''
 
-number = input('Введите многозначное число: ')
-even = 0
-odd = 0
-all = 0
-
-for i in number:
-    if int(i) % 2 == 0:
+def func(number, even=0, odd=0, all=0):
+    if number == 0:
+        return even, odd, all
+    if number % 2 == 0:
         even += 1
         all += 1
     else:
         odd += 1
         all += 1
-print(f'Количество чётных цифр в числе: {even}\n'
-      f'Количество нечётных цифр в числе: {odd}\n'
-      f'Всего цифр в числе: {all}')
+    number = number // 10   # Не понял, почему без этого не работает и показывает превышение глубины рекурсии.
+    return func(number, even, odd, all)
+
+num = int(input('Введите многозначное число: '))
+print(func(num))
