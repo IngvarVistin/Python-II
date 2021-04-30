52a8c3973c7aa0eba52121cc8b7e1f5c670d45c8
Ingvar <Dimon_chick@mail.ru>
2021-04-30 22:20:09 +0900

    II Lesson_2_2 href

--- Lesson__2/II Lesson_2_2.py
+++ Lesson__2/II Lesson_2_2.py
@@ -2,7 +2,7 @@
 2. Посчитать четные и нечетные цифры введенного натурального числа.
 Например, если введено число 34560, то у него 3 четные цифры (4, 6 и 0) и 2 нечетные (3 и 5).
 '''
-
+# https://drive.google.com/file/d/1d-A2fhc8saMYFs1HXnhnEAFCOtuPeeMg/view?usp=sharing
 def func(number, even=0, odd=0, all=0):
     if number == 0:
         return even, odd, all
